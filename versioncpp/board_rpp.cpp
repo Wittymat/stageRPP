4fec068c06315c98fff8fcda119cf57262054f0e
Swarm <ruben.patriarca@etu.univ-paris-diderot.fr>
2021-05-27 10:56:01 +0200

    maj

--- versioncpp/board_rpp.cpp
+++ versioncpp/board_rpp.cpp
@@ -8,9 +8,10 @@ using namespace std;
 
 const int MaxPlayoutLength = 500000;
 const int MaxLegalMoves = 1000;
-const int MaxMoveNumber = 100000;
 const bool closed = true;
 Graph g = read_graph("/Users/swarm/Documents/stage/URPP/UR132");
+const int MaxMoveNumber = g.nb_vertices*g.nb_vertices;
+
 
 typedef int Move[2];
 
